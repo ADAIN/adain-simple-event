1cc8cd12c6c786b9e0d3ebecaca4e3330df5f2de
SungYong Jang <jsy@adain.kr>
2019-02-22 10:03:47 +0900

    remove Map - Map has problem in IE

--- src/index.js
+++ src/index.js
@@ -1,52 +1,36 @@
-export const SimpleEvent = {};
-SimpleEvent.items = new Map();
-SimpleEvent.eventTypeWithIds = new Map();
+import _ from 'lodash';
+
+const SimpleEvent = {};
+SimpleEvent.items = {};
+SimpleEvent.eventTypeWithIds = {};
 SimpleEvent.counter = 0;
 SimpleEvent.dispatch = function(eventType, data){
-  if(SimpleEvent.items.has(eventType)){
-    const eventTypeMap = SimpleEvent.items.get(eventType);
-    eventTypeMap.forEach((item, key)=>{
-      try{
-        if(item.context){
-          item.handler.call(item.context, data);
-        }else{
-          item.handler(data);
-        }
-      }catch(e){
-        console.error(`SimpleEventError(type=${eventType}, key=${key})`, e);
-      }
-    });
-  }
+  _.forEach(SimpleEvent.items[eventType], (item)=>{
+    if(item.context){
+      item.handler.call(item.context, data);
+    }else{
+      item.handler(data);
+    }
+  });
 };
 
 SimpleEvent.register = function(eventType, handler, context){
-  const id = SimpleEvent.counter++;
-  if(!SimpleEvent.items.has(eventType)){
-    SimpleEvent.items.set(eventType, new Map());
+  let id = SimpleEvent.counter++;
+  if(!SimpleEvent.items[eventType]){
+    SimpleEvent.items[eventType] = {};
   }
 
-  const eventTypeMap = SimpleEvent.items.get(eventType);
-  eventTypeMap.set(id, {eventType, handler, context});
-  SimpleEvent.eventTypeWithIds.set(id, eventType);
+  SimpleEvent.items[eventType][id] = {eventType, handler, context};
+  SimpleEvent.eventTypeWithIds[id] = eventType;
   return id;
 };
 
 SimpleEvent.unRegister = function(id){
-  const eventType = SimpleEvent.eventTypeWithIds.get(id);
-  const listenerMap = SimpleEvent.items.get(eventType);
-  if(!listenerMap){
-    return;
-  }
-  if(listenerMap.has(id)){
-    listenerMap.delete(id);
-  }
-  if(listenerMap.size === 0){
-    SimpleEvent.items.delete(eventType);
-  }
+  delete SimpleEvent.items[SimpleEvent.eventTypeWithIds[id]][id];
 };
 
 SimpleEvent.unRegisterWithArr = function(ids){
-  ids.forEach((id)=>{
+  _.each(ids, (id)=>{
     SimpleEvent.unRegister(id);
   });
 };
