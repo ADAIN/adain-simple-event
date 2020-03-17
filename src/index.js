4f1a96c3bd8aa40e038fdf9cd512353a6d5b9cf0
SungYong Jang <jsy@adain.kr>
2020-03-17 11:57:23 +0900

    add debug feature

--- src/index.js
+++ src/index.js
@@ -1,47 +1,50 @@
-const SimpleEvent = {};
-SimpleEvent.items = {};
-SimpleEvent.eventTypeWithIds = {};
-SimpleEvent.counter = 0;
-SimpleEvent.dispatch = function(eventType, data){
-  const keys = Object.keys(SimpleEvent.items[eventType]);
-  const length = keys.length;
-  let item;
-  for (let i = 0; i < length; i++) {
-    item = SimpleEvent.items[eventType][keys[i]];
-    if(item === undefined){
-      return;
+export const SimpleEvent = {
+  isDebug: false,
+  warningCount: 5,
+  items: {},
+  eventTypeWithIds: {},
+  counter: 0,
+  dispatch(eventType, data){
+    const keys = Object.keys(SimpleEvent.items[eventType]);
+    const length = keys.length;
+    let item;
+    for (let i = 0; i < length; i++) {
+      item = SimpleEvent.items[eventType][keys[i]];
+      if(item === undefined){
+        return;
+      }
+      if(item.context){
+        item.handler.call(item.context, data);
+      }else{
+        item.handler(data);
+      }
     }
-    if(item.context){
-      item.handler.call(item.context, data);
-    }else{
-      item.handler(data);
+  },
+  register(eventType, handler, context){
+    let id = SimpleEvent.counter++;
+    if(!SimpleEvent.items[eventType]){
+      SimpleEvent.items[eventType] = {};
     }
-  }
-};
 
-SimpleEvent.register = function(eventType, handler, context){
-  let id = SimpleEvent.counter++;
-  if(SimpleEvent.keys.indexOf(eventType) === -1){
-    SimpleEvent.keys.push(eventType);
-  }
+    SimpleEvent.items[eventType][id] = {eventType, handler, context};
+    SimpleEvent.eventTypeWithIds[id] = eventType;
 
-  if(!SimpleEvent.items[eventType]){
-    SimpleEvent.items[eventType] = {};
-  }
-
-  SimpleEvent.items[eventType][id] = {eventType, handler, context};
-  SimpleEvent.eventTypeWithIds[id] = eventType;
-  return id;
-};
-
-SimpleEvent.unRegister = function(id){
-  delete SimpleEvent.items[SimpleEvent.eventTypeWithIds[id]][id];
-};
-
-SimpleEvent.unRegisterWithArr = function(ids){
-  const length = ids.length;
-  for (let i = 0; i < length; i++) {
-    SimpleEvent.unRegister(ids[i]);
+    if(
+      SimpleEvent.isDebug &&
+      Object.keys(SimpleEvent.items[eventType]).length > SimpleEvent.warningCount
+    ){
+      console.warn(`SimpleEvent EventType[${eventType}] registered count is more than ${SimpleEvent.warningCount}. Please check out!`);
+    }
+    return id;
+  },
+  unRegister(id){
+    delete SimpleEvent.items[SimpleEvent.eventTypeWithIds[id]][id];
+  },
+  unRegisterWithArr(ids){
+    const length = ids.length;
+    for (let i = 0; i < length; i++) {
+      SimpleEvent.unRegister(ids[i]);
+    }
   }
 };
 
